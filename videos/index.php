2b10b450bb37d8023e1081233bea47a14bb9477f
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-08-02 04:27:56 -0600

    Update index.php
    If no thumbnail exists, it now displays an image that says there is no thumbnail, rather than displaying an aurora image.
    I discussed this with Thomas the end of July, 2021.
    
    Fixed issue where thumbnails were stretched horizontally on large screens.
    
    Removed duplicate image name code.
    
    Left in the legacy "onmouseenter" and "onmouseleave" code, but it's not used.  Thomas can determine if it should be removed (remove $y and <script> block).

--- videos/index.php
+++ videos/index.php
@@ -1,8 +1,8 @@
 <html>
 	<head>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-        <script src="../analyticstracking.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<script src="../analyticsTracking.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<link href="../allsky.css" rel="stylesheet">
 	</head>
 	<body>
@@ -27,27 +27,34 @@
 		echo "<a class='back-button' href='..'><i class='fa fa-chevron-left'></i>Back to Live View</a>";
 		echo "<div class=archived-videos>";
 
+		$no_thumbnail_image = "../NoThumbnail.png";
 		foreach ($files as $file) {
 			$year = substr($file, 7, 4);
 			$month = substr($file, 11, 2);
 			$day = substr($file, 13, 2);
 			$date = $year.$month.$day;
-			if (file_exists("thumbnails/".$date.".gif")){
-				//echo "<img style='display:none' src='thumbnails/".$date.".gif'>";
-				echo "<a href='./$file'><div class='day-container'><div class='image-container'><img id=".$date." src='../aurora-snap.jpg' title='$year-$month-$day' onmouseenter='onImgEnter(this)' onmouseleave='onImgLeave(this)'/></div><div>$year-$month-$day</div></div></a>";
+			// put in variable so we don't duplicate below.
+			$thumbnail = "thumbnails/".$date.".gif";
+			$x = "<a href='./$file'><div class='day-container'><div class='image-container'><img id='$date'";
+			$y = "onmouseenter='onImgEnter(this)' onmouseleave='onImgLeave(this)'";	// LEGACY - NOT NEEDED
+			$y = "";
+			$z = "title='$year-$month-$day' /></div><div>$year-$month-$day</div></div></a>";
+			if (file_exists($thumbnail)){
+				//echo "<img style='display:none' src='".$thumbnail."'>";
+				echo $x." src='$thumbnail' ".$y." ".$z;
 			} else {
-				echo "<a href='./$file'><div class='day-container'><div class='image-container'><img id=".$date." src='../aurora-snap.jpg' title='$year-$month-$day'/></div><div>$year-$month-$day</div></div></a>";
+				// On large screens, the thumbnails are stretched width-wise, so set width to auto.
+				echo $x." src='$no_thumbnail_image' style='width: auto' ".$z;
 			}
-			
 		}
 		echo "</div>";
 		?>
 		<script>
 			function onImgEnter(img) {
-				$(img).attr("src", "thumbnails/" + $(img).attr("id") + ".gif");              
+				$(img).attr("src", "thumbnails/" + $(img).attr("id") + ".gif");
 			}
 			function onImgLeave(img) {
-				$(img).attr("src", "../aurora-snap.jpg");              
+				$(img).attr("src", "<?php echo "$no_thumbnail_image"; ?>");
 			}
 		</script>
 	</body>
