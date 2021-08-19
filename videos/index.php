dd133c64a4286afb33f67feee5933cb8e8b42267
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-08-19 01:12:33 -0500

    Put functionality in functions.php
    See description of similar change for startrails/index.php

--- videos/index.php
+++ videos/index.php
@@ -7,55 +7,8 @@
 	</head>
 	<body>
 		<?php
-
-		$files = array();
-		if ($handle = opendir('.')) {
-
-			while (false !== ($entry = readdir($handle))) {
-
-				if (strpos($entry, 'mp4') !== false) {
-
-					$files[] = $entry;
-				}
-			}
-
-			closedir($handle);
-		}
-
-		asort($files);
-
-		echo "<a class='back-button' href='..'><i class='fa fa-chevron-left'></i>Back to Live View</a>";
-		echo "<div class=archived-videos>";
-
-		$no_thumbnail_image = "../NoThumbnail.png";
-		foreach ($files as $file) {
-			$year = substr($file, 7, 4);
-			$month = substr($file, 11, 2);
-			$day = substr($file, 13, 2);
-			$date = $year.$month.$day;
-			// put in variable so we don't duplicate below.
-			$thumbnail = "thumbnails/".$date.".gif";
-			$x = "<a href='./$file'><div class='day-container'><div class='image-container'><img id='$date'";
-			$y = "onmouseenter='onImgEnter(this)' onmouseleave='onImgLeave(this)'";	// LEGACY - NOT NEEDED
-			$y = "";
-			$z = "title='$year-$month-$day' /></div><div>$year-$month-$day</div></div></a>";
-			if (file_exists($thumbnail)){
-				//echo "<img style='display:none' src='".$thumbnail."'>";
-				echo $x." src='$thumbnail' ".$y." ".$z;
-			} else {
-				// On large screens, the thumbnails are stretched width-wise, so set width to auto.
-				echo $x." src='$no_thumbnail_image' style='width: auto' ".$z;
-			}
-		}
-		echo "</div>";
+		include '../functions.php';
+		display_thumbnails("allsky");
 		?>
-		<script>
-			function onImgEnter(img) {
-				$(img).attr("src", "thumbnails/" + $(img).attr("id") + ".gif");
-			}
-			function onImgLeave(img) {
-				$(img).attr("src", "<?php echo "$no_thumbnail_image"; ?>");
-			}
-		</script>
 	</body>
 </html>
