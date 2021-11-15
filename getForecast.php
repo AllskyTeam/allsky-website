811a8a0bbe590c7595f8f44df971973e80a130ed
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-11-15 15:52:57 -0600

    getForecast.php: ignore warning messages
    Don't have the web server log an error if we can't get to the URL since we're already notifying the user.
    Putting a message in the web log file just clutters it up.

--- getForecast.php
+++ getForecast.php
@@ -1,11 +1,12 @@
 <?php
 $url = "http://services.swpc.noaa.gov/text/3-day-forecast.txt";
+// Don't want warning messages to go into the web log for things like the server is down.
+// We display a message to the user which is sufficient.
+error_reporting(E_ALL ^ E_WARNING);
 $forecast   = file_get_contents($url);
 if ($forecast != "") {
     $stripStart = substr($forecast, strpos($forecast, "00-03UT"));
     $kpTable    = substr($stripStart, 0, strpos($stripStart, "Rationale") -2 );
-    //echo $kpTable;
-
     $rows       = explode("\n", $kpTable);
 
     foreach($rows as $row => $data)
