b93b7bc0b871a209afc5702114648605564d3912
Michael J. Kidd <linuxkidd@gmail.com>
2022-02-01 21:22:16 -0800

    Add support for jpeg|png for images and webm for video

--- functions.php
+++ functions.php
@@ -58,18 +58,24 @@ function make_thumb($src, $dest, $desired_width)
 {
  	/* Make sure the imagecreatefromjpeg() function is in PHP. */
 	global $displayed_thumbnail_error_message;
-	if (function_exists('imagecreatefromjpeg') == false)
+	if ( preg_match("/^.*\.(jpg|jpeg)$/", $src ) ) {
+		$funcext='jpeg';
+	} elseif ( preg_match("/^.*\.png$/", $src ) ) {
+		$funcext='png';
+	}
+	if (function_exists("imagecreatefrom${funcext}") == false)
 	{
 		if ($displayed_thumbnail_error_message == false)
 		{
-			echo "<br><p style='color: red'>Unable to make thumbnail(s); imagecreatefromjpeg() does not exist.<br>If you do NOT have the file '/etc/php/7.3/mods-available/gd.ini' you need to download the latest PHP.</p>";
+			echo "<br><p style='color: red'>Unable to make thumbnail(s); imagecreatefrom{$funcext}() does not exist.<br>If you do NOT have the file '/etc/php/7.3/mods-available/gd.ini' you need to download the latest PHP.</p>";
 			$displayed_thumbnail_error_message = true;
 		}
 		return(false);
 	}
 
 	/* read the source image */
-	$source_image = imagecreatefromjpeg($src);
+	$funcname="imagecreatefrom{$funcext}";
+	$source_image = $funcname($src);
 	$width = imagesx($source_image);
 	$height = imagesy($source_image);
 
@@ -83,7 +89,8 @@ function make_thumb($src, $dest, $desired_width)
 	imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
 
 	/* create the physical thumbnail image to its destination */
- 	imagejpeg($virtual_image, $dest);
+	$funcname="image{$funcext}";
+ 	$funcname($virtual_image, $dest);
 
 	if (file_exists($dest)) {
 		return(true);
@@ -113,16 +120,16 @@ function display_thumbnails($image_type)
 	global $back_button;
 	$image_type_len = strlen($image_type);
 	if ($image_type == "Timelapse") {
-		$ext = "mp4";
+		$ext = "/^.*\.(mp4|webm)$/";
 	} else {
-		$ext = "jpg";
+		$ext = "/^.*\.(jpg|jpeg|png)$/";
 	}
 
 	$num_files = 0;
 	$files = array();
 	if ($handle = opendir('.')) {
 		while (false !== ($entry = readdir($handle))) {
-			if (strpos($entry, $ext) !== false) {
+			if ( preg_match( $ext, $entry ) ) {
 				$files[] = $entry;;
 				$num_files++;
 			}
