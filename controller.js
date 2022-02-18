e8505a18fb27f9ee5f75caab08baf13dd535481d
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-02-17 23:25:55 -0600

    controller.js: don't get forecast if not needed
    If the user set "auroraForecast" to false, then don't call getForecast.php to get the forecast since it won't be used.

--- controller.js
+++ controller.js
@@ -490,38 +490,40 @@ function AppCtrl($scope, $timeout, $http, _) {
 		return scale[index];
 	};
 
+	if ($scope.auroraForecast) {
 	$scope.getForecast = function () {
 
-		function getSum(data, field) {
-			var total = _.sumBy(data, function (row) {
-				return parseInt(row[field]);
-			});
-			return Math.round(total / 7);
-		}
+			function getSum(data, field) {
+				var total = _.sumBy(data, function (row) {
+					return parseInt(row[field]);
+				});
+				return Math.round(total / 7);
+			}
 
-		function getDay(number) {
-			var day = moment().add(number, 'd');
-			return moment(day).format("MMM") + " " + moment(day).format("DD");
-		}
+			function getDay(number) {
+				var day = moment().add(number, 'd');
+				return moment(day).format("MMM") + " " + moment(day).format("DD");
+			}
 
-		$http.get("getForecast.php")
-			.then(function (response) {
-				$scope.forecast = {};
-				// If the 1st 'time' value begins with "ERROR", there was an error getting data.
-				msg = response.data[0]['time'];
-				if ((msg.substring(0,9) == "WARNING: ") || response.data == "") {
-					// 100 indicates warning
-					$scope.forecast[''] = 100;	// displays "WARNING"
-					$scope.forecast[msg.substring(9)] = -1; // displays msg
-				} else {
-					$scope.forecast[getDay(0)] = getSum(response.data, "day1");
-					$scope.forecast[getDay(1)] = getSum(response.data, "day2");
-					$scope.forecast[getDay(2)] = getSum(response.data, "day3");
-				}
-			});
-	};
+			$http.get("getForecast.php")
+				.then(function (response) {
+					$scope.forecast = {};
+					// If the 1st 'time' value begins with "WARNING", there was an error getting data.
+					msg = response.data[0]['time'];
+					if ((msg.substring(0,9) == "WARNING: ") || response.data == "") {
+						// 100 indicates warning
+						$scope.forecast[''] = 100;	// displays "WARNING"
+						$scope.forecast[msg.substring(9)] = -1; // displays msg
+					} else {
+						$scope.forecast[getDay(0)] = getSum(response.data, "day1");
+						$scope.forecast[getDay(1)] = getSum(response.data, "day2");
+						$scope.forecast[getDay(2)] = getSum(response.data, "day3");
+					}
+				});
+		};
 
-	$scope.getForecast();
+		$scope.getForecast();
+	}
 }
 
 
