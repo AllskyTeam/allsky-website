2c074c0b7e775cba34afea50878b38222b6ba02a
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-03-19 00:40:47 -0500

    index.php: add common HTML tags

--- keograms/index.php
+++ keograms/index.php
@@ -1,6 +1,12 @@
 <?php include '../functions.php'; disableBuffering(); // must be first line ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
+		<meta http-equiv="Content-Type" content="text/html" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" type="image/png" href="../allsky-favicon.png">
+		<title>Keograms</title>
+
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<script src="../analyticsTracking.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
