ea513c7948d850d770ef3d18d7558ee77a4c27b1
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-04-07 12:11:22 -0600

    index.php: conditional analyticsTracking.js
    * Only include `analyticsTracking.js` if the user modified it.
    * We distribute it with a message that less than 50 characters:       // Include your Google Analytics code here

--- keograms/index.php
+++ keograms/index.php
@@ -8,8 +8,10 @@
 		<title>Keograms</title>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<script src="../analyticsTracking.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<?php if (file_exists("../analyticsTracking.js") && filesize("../analyticsTracking.js") > 50) { ?>
+		<script src="../analyticsTracking.js"></script>
+<?php } ?>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<link href="../allsky.css" rel="stylesheet">
 	</head>
 	<body>
