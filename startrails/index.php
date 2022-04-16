769c2f53c2b932799a3ca5d308b854494cb33414
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-04-15 20:26:29 -0600

    index.php: bug fix

--- startrails/index.php
+++ startrails/index.php
@@ -6,7 +6,7 @@
 		<meta http-equiv="Content-Type" content="text/html" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="shortcut icon" type="image/png" href="../allsky-favicon.png">
-		<title><? echo $type; ?></title>
+		<title><?php echo $type; ?></title>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <?php if (file_exists("../analyticsTracking.js") && filesize("../analyticsTracking.js") > 50) { ?>
