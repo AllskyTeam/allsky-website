d0981851e837c14793cbe4da6f8a792a8b746d3b
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-06-19 05:47:33 -0500

    functions.php: fix $onPi setting
    * Fix $onPi setting - the ".['onPi']" shouldn't have been there.
    * Improve the message when it appears the software hasn't been configured to take into account remote machines.

--- functions.php
+++ functions.php
@@ -9,6 +9,7 @@ function v($var, $default, $a) {
 	else return($default);
 }
 
+$configurationFileName = "configuration.json";
 if (isset($_GET['onPi'])) {
 	$onPi = $_GET['onPi'];
 } else {
@@ -16,7 +17,7 @@ if (isset($_GET['onPi'])) {
 	// Some settings impact this page, some impact the constellation overlay.
 	if (! isset($configFilePrefix))
 		$configFilePrefix = "";
-	$configuration_file = $configFilePrefix . "configuration.json";
+	$configuration_file = $configFilePrefix . $configurationFileName;
 	if (! file_exists($configuration_file)) {
 		echo "<p style='color: red; font-size: 200%'>";
 		echo "ERROR: Missing configuration file '$configuration_file'.  Cannot continue.";
@@ -27,14 +28,13 @@ if (isset($_GET['onPi'])) {
 	$settings_array = json_decode($settings_str, true);
 	if ($settings_array == null) {
 		echo "<p style='color: red; font-size: 200%'>";
-		echo "ERROR: Bad configuration file '<span style='color: black'>$configuration_file</span>'.  Cannot continue.";
+		echo "ERROR: Bad configuration file '<span style='color: black'>$configurationFileName</span>'.  Cannot continue.";
 		echo "<br>Check for missing quotes or commas at the end of every line (except the last one).";
 		echo "</p>";
 		echo "<pre>$settings_str</pre>";
 		exit;
 	}
-	$onPi = v("onPi", true, $settings_array['homePage'].['onPi']);
-
+	$onPi = v("onPi", true, $settings_array['homePage']);
 }
 
 // If on a Pi, check that the placeholder was replaced.
@@ -42,9 +42,16 @@ if ($onPi && ALLSKY_CONFIG == "XX_ALLSKY_CONFIG" . "_XX") {
 	// This file hasn't been updated yet after installation.
 	echo "<div style='font-size: 200%;'>";
 	echo "<span style='color: red'>";
-	echo "Please run the following from the 'allsky' directory before using the Website:";
+	echo "If this Website is running on a Pi, please run the following:";
 	echo "</span>";
-	echo "<br><br><code>   website/install.sh --update</code>";
+	echo "<br><code>";
+	echo "&nbsp; &nbsp; &nbsp; &nbsp; cd ~/allsky";
+	echo "<br>&nbsp; &nbsp; &nbsp; &nbsp; website/install.sh --update";
+	echo "</code>";
+
+	echo "<span style='color: red'>";
+	echo "<br><br>If instead, this Website is being run on a remote server, change the <b>onPi</b> variable in the '$configurationFileName' configuration file to <b>false</b>.";
+	echo "</span>";
 	echo "</div>";
 	exit;
 }
