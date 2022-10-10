102bf8db0196ac9b5084c663a495da9da92cc5b9
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2022-10-10 15:59:11 -0500

    functions.php: bug fix: typo

--- functions.php
+++ functions.php
@@ -256,7 +256,7 @@ function display_thumbnails($dir, $file_prefix, $title)
 	
 	$thumb_dir = "$dir/thumbnails";
 	if (! is_dir($thumb_dir)) {
-		if (! mkdir($thum_dir, 0775))
+		if (! mkdir($thumb_dir, 0775))
 			echo "<p>Unable to make '$thum_dir' directory. You will need to create it manually.</p>";
 			print_r(error_get_last());
 	}
