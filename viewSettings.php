185467b7ad463745ab32c64d5375f445e026e557
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2023-01-06 22:08:16 -0600

    viewSettings.php: make bootstrap.min.css a link
    so it's one less file to upload.

--- viewSettings.php
+++ viewSettings.php
@@ -45,9 +45,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="shortcut icon" type="<?php echo $faviconType ?>" href="<?php echo $favicon ?>">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+		rel="stylesheet"
+		integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+		crossorigin="anonymous">
+
 <?php
 	if ($fullMode) {
-		echo "<link rel='stylesheet' type='text/css' href='$vSDir/bootstrap.min.css'>";
 		echo "<link rel='stylesheet' type='text/css' href='$vSDir/custom.css'>";
 	} else {
 		echo "<link rel='stylesheet' type='text/css' href='allsky.css'>";
